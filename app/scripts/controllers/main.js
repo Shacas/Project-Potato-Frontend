d9f47d391b8f0f7b32ef74eadbd64910b9c0cac3
David Jiang <SharkIng@Shacas.com>
2016-02-02 15:04:46 -0500

    Show case

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,21 @@
  * Controller of the potatoApp
  */
 angular.module('potatoApp')
-  .controller('MainCtrl', function ($scope, $http, ngCart) {
+  .controller('MainCtrl', function ($scope, $http, ngCart, $sails) {
     ngCart.setTaxRate(13.0);
     ngCart.setShipping(5.99);
+
+    $scope.items = [];
+    $sails.get("/product")
+        .success(function (data, status, headers, jwr) {
+          $scope.items = data;
+      })
+        .error(function (data, status, headers, jwr) {
+          console.log(status);
+      });
+
+      $scope.getItems = function() {
+        return $scope.items;
+      };
+
   });
